f62349c119102da2295fa3933f5ee69ba9d06fe8
aiq <alex.schulz.mail@googlemail.com>
2022-02-05 11:43:14 +0100

    added ReadError documentation and added DeeperAs function

--- error.go
+++ error.go
@@ -2,29 +2,32 @@ package tok
 
 import "fmt"
 
+// Error type that ReadFunc and the Reader here return.
 type ReadError struct {
 	What string
 	Line int
 	Col  int
 }
 
+func (e ReadError) DeeperAs(oth ReadError) bool {
+	return e.Line >= oth.Line && e.Col > oth.Col
+}
+
 func (e ReadError) Error() string {
-	return fmt.Sprintf("not able to read %q at line:%d col:%d", e.What, e.Line, e.Col)
+	return fmt.Sprintf("not able to read %s at line:%d col:%d", e.What, e.Line, e.Col)
 }
 
-func MakeReadError(what string, line int, col int) ReadError {
-	return ReadError{what, line, col}
-}
-
+// Generates a ReadError for name.
 func (s *Scanner) ErrorFor(name string) error {
 	l, c := s.LineCol()
-	return fmt.Errorf("not able to read %q at line:%d col:%d", name, l, c)
+	return fmt.Errorf("not able to read %s at line:%d col:%d", name, l, c)
 }
 
+// Generates a ErrorFor if ok is false, otherwise returns the function nil.
 func (s *Scanner) BoolErrorFor(ok bool, name string) error {
 	if !ok {
 		l, c := s.LineCol()
-		return MakeReadError(name, l, c)
+		return ReadError{name, l, c}
 	}
 	return nil
 }
