db2ef05eca5cf5cc98498a979e2ca2a8c299bc93
aiq <alex.schulz.mail@googlemail.com>
2022-02-06 21:08:02 +0100

    added one line documentation

--- token.go
+++ token.go
@@ -2,12 +2,13 @@ package tok
 
 import "fmt"
 
-// Token marks a sub string.
+// Token marks a sub string in a Scanner.
 type Token struct {
 	from Marker
 	to   Marker
 }
 
+// Norm mormalizes a Token.
 func (t Token) Norm() Token {
 	if t.from > t.to {
 		t.from, t.to = t.to, t.from
@@ -15,26 +16,25 @@ func (t Token) Norm() Token {
 	return t
 }
 
+// Equal reports if t and oth mark the same sub string in a Scanner.
 func (t Token) Equal(oth Token) bool {
 	t, oth = t.Norm(), oth.Norm()
 	return t.from == oth.from && t.to == oth.to
 }
 
-func (t Token) Has(oth Token) bool {
-	t, oth = t.Norm(), oth.Norm()
-	return t.from <= oth.from && t.to >= oth.to
+// Has returns true if sub is a sub Token of t.
+func (t Token) Has(sub Token) bool {
+	t, sub = t.Norm(), sub.Norm()
+	return t.from <= sub.from && t.to >= sub.to
 }
 
-func (t Token) Split(oth Token) (Token, Token) {
-	t, oth = t.Norm(), oth.Norm()
-	return Token{t.from, oth.from}, Token{oth.to, t.to}
+// Split splits a Token into two parts via sep.
+func (t Token) Split(sep Token) (Token, Token) {
+	t, sep = t.Norm(), sep.Norm()
+	return Token{t.from, sep.from}, Token{sep.to, t.to}
 }
 
-func (t Token) Len() int {
-	t = t.Norm()
-	return int(t.to - t.from)
-}
-
+// String returns a readable representation of a Token.
 func (t Token) String() string {
 	return fmt.Sprintf("[%d-%d)", t.from, t.to)
 }
@@ -56,18 +56,21 @@ func (s *Scanner) Tokenize(f ScopeFunc) (Token, bool) {
 	return Token{a, b}.Norm(), res
 }
 
+// Segment groups a Token with the sub string from a Scanner.
 type Segment struct {
 	FromToken bool
 	Token
 	Value string
 }
 
+// Equal reports if seg and oth mark are equal.
 func (seg Segment) Equal(oth Segment) bool {
 	return seg.FromToken == oth.FromToken &&
 		seg.Token.Equal(oth.Token) &&
 		seg.Value == oth.Value
 }
 
+// String returns a readable representation of a Segment.
 func (seg Segment) String() string {
 	from := ""
 	if seg.FromToken {
@@ -76,6 +79,7 @@ func (seg Segment) String() string {
 	return fmt.Sprintf("%s%s%q", seg.Token.String(), from, seg.Value)
 }
 
+// Segmentate splits the full string of a Scanner into segments.
 func (s *Scanner) Segmentate(tokens []Token) ([]Segment, error) {
 	res := []Segment{}
 	left := Token{}
