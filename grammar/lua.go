ba4d3baad911e76f92c5e973c924983c6f689277
aiq <alex.schulz.mail@googlemail.com>
2022-02-20 21:37:35 +0100

    no need to use Lit and Rune in this cases

--- grammar/lua.go
+++ grammar/lua.go
@@ -40,7 +40,6 @@ type LuaReader struct {
 // Lua creates a Grammar to Read a Lua file.
 // The implementation is based on https://www.lua.org/manual/5.4/manual.html#9
 func Lua() *LuaReader {
-	r := Rune
 	g := &LuaReader{}
 	SetRuleNames(g)
 	g.Name.Reader = Lit("a")
@@ -53,59 +52,63 @@ func Lua() *LuaReader {
 	)
 	g.FieldSep.Reader = AnyRune(",;")
 	g.Field.Reader = Any(
-		Seq(r('['), &g.Exp, r(']'), r('='), &g.Exp),
-		Seq(&g.Name, r('='), &g.Exp),
+		Seq('[', &g.Exp, ']', '=', &g.Exp),
+		Seq(&g.Name, '=', &g.Exp),
 		&g.Exp,
 	)
 	g.FieldList.Reader = Seq(&g.Field, Zom(Seq(&g.FieldSep, &g.Field)), Opt(&g.FieldSep))
-	g.TableConstructor.Reader = Seq(r('{'), Opt(&g.FieldList), r('}'))
-	g.ParList.Reader = Any(Seq(&g.NameList, Opt(Seq(r(','), Lit("...")))), Lit("..."))
-	g.FuncBody.Reader = Seq(r('('), &g.ParList, r(')'), &g.Block, Lit("end"))
+	g.TableConstructor.Reader = Seq('{', Opt(&g.FieldList), '}')
+	g.ParList.Reader = Any(Seq(&g.NameList, Opt(Seq(',', "..."))), "...")
+	g.FuncBody.Reader = Seq('(', &g.ParList, ')', &g.Block, "end")
 	g.FunctionDef.Reader = Seq(Lit("function"), &g.FuncBody)
 	g.Args.Reader = Any(
-		Seq(r('('), &g.ExpList, r(')')),
+		Seq('(', &g.ExpList, ')'),
 		&g.TableConstructor,
 		&g.LiteralString,
 	)
 	g.FunctionCall.Reader = Any(
 		Seq(&g.PrefixExp, &g.Args),
-		Seq(&g.PrefixExp, r(':'), &g.Name, &g.Args),
+		Seq(&g.PrefixExp, ':', &g.Name, &g.Args),
 	)
-	g.PrefixExp.Reader = Any(&g.Var, &g.FunctionCall, Seq(r('('), &g.Exp, r(')')))
+	g.PrefixExp.Reader = Any(&g.Var, &g.FunctionCall, Seq('(', &g.Exp, ')'))
 	g.Exp.Reader = Any(
-		Lit("nil"), Lit("false"), Lit("true"), &g.Numeral, &g.LiteralString, Lit("..."),
+		"nil", "false", "true", &g.Numeral, &g.LiteralString, "...",
 		&g.FunctionDef, &g.PrefixExp, &g.TableConstructor,
 		Seq(&g.Exp, &g.BinOp, &g.Exp), Seq(&g.UnOp, &g.Exp),
 	)
-	g.ExpList.Reader = Seq(&g.Exp, Zom(Seq(r(','), &g.Exp)))
-	g.NameList.Reader = Seq(&g.Name, Zom(Seq(r(','), &g.Name)))
+	g.ExpList.Reader = Seq(&g.Exp, Zom(Seq(',', &g.Exp)))
+	g.NameList.Reader = Seq(&g.Name, Zom(Seq(',', &g.Name)))
 	g.Var.Reader = Any(
 		&g.Name,
-		Seq(&g.PrefixExp, r('['), &g.Exp, r(']')),
-		Seq(&g.PrefixExp, r('.'), &g.Name),
+		Seq(&g.PrefixExp, '[', &g.Exp, ']'),
+		Seq(&g.PrefixExp, '.', &g.Name),
 	)
-	g.VarList.Reader = Seq(&g.Var, Zom(Seq(r(','), &g.Var)))
-	g.FuncName.Reader = Seq(&g.Name, Zom(Seq(r('.'), &g.Name)), Opt(Seq(r(':'), &g.Name)))
+	g.VarList.Reader = Seq(&g.Var, Zom(Seq(',', &g.Var)))
+	g.FuncName.Reader = Seq(&g.Name, Zom(Seq('.', &g.Name)), Opt(Seq(':', &g.Name)))
 	g.Label.Reader = Seq(Lit("::"), &g.Name, Lit("::"))
-	g.RetStat.Reader = Seq(Lit("return"), Opt(&g.ExpList), Opt(r(';')))
-	g.Attrib.Reader = Opt(Seq(r('<'), &g.Name, r('>')))
-	g.AttNameList.Reader = Seq(&g.Name, &g.Attrib, Zom(Seq(r(','), &g.Name, &g.Attrib)))
+	g.RetStat.Reader = Seq(Lit("return"), Opt(&g.ExpList), Opt(';'))
+	g.Attrib.Reader = Opt(Seq('<', &g.Name, '>'))
+	g.AttNameList.Reader = Seq(&g.Name, &g.Attrib, Zom(Seq(',', &g.Name, &g.Attrib)))
 	g.Stat.Reader = Any(
-		r(';'),
-		Seq(&g.VarList, r('='), &g.ExpList),
+		';',
+		Seq(&g.VarList, '=', &g.ExpList),
 		&g.FunctionCall,
 		&g.Label,
-		Lit("break"),
-		Seq(Lit("goto"), &g.Name),
-		Seq(Lit("do"), &g.Block, Lit("end")),
-		Seq(Lit("while"), &g.Exp, Lit("do"), &g.Block, Lit("end")),
-		Seq(Lit("repeat"), &g.Block, Lit("until"), &g.Exp),
-		Seq(Lit("if"), &g.Exp, Lit("then"), &g.Block, Zom(Seq(Lit("elseif"), &g.Exp, Lit("then"), &g.Block)), Opt(Seq(Lit("else"), &g.Block)), Lit("end")),
-		Seq(Lit("for"), &g.Name, r('='), &g.Exp, r(','), &g.Exp, Opt(Seq(r(','), &g.Exp)), Lit("do"), &g.Block, Lit("end")),
-		Seq(Lit("for"), &g.NameList, Lit("in"), &g.ExpList, Lit("do"), &g.Block, Lit("end")),
-		Seq(Lit("function"), &g.FuncName, &g.FuncBody),
-		Seq(Lit("local"), Lit("function"), &g.Name, &g.FuncBody),
-		Seq(Lit("local"), &g.AttNameList, Opt(Seq(r('='), &g.ExpList))),
+		"break",
+		Seq("goto", &g.Name),
+		Seq("do", &g.Block, "end"),
+		Seq("while", &g.Exp, "do", &g.Block, "end"),
+		Seq("repeat", &g.Block, "until", &g.Exp),
+		Seq("if", &g.Exp, "then", &g.Block,
+			Zom(Seq("elseif", &g.Exp, "then", &g.Block)),
+			Opt(Seq("else", &g.Block)),
+			"end",
+		),
+		Seq("for", &g.Name, '=', &g.Exp, ',', &g.Exp, Opt(Seq(',', &g.Exp)), "do", &g.Block, "end"),
+		Seq("for", &g.NameList, "in", &g.ExpList, "do", &g.Block, "end"),
+		Seq("function", &g.FuncName, &g.FuncBody),
+		Seq("local", "function", &g.Name, &g.FuncBody),
+		Seq("local", &g.AttNameList, Opt(Seq('=', &g.ExpList))),
 	)
 	g.Block.Reader = Seq(Zom(&g.Stat), Opt(&g.RetStat))
 	g.Chunk.Reader = &g.Block
